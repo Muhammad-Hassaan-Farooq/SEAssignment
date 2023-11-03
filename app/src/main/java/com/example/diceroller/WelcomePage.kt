90df6bb964646b37554d1a152eb250b8bf233ccb
Muhammad-Hassaan-Farooq <101121986+Muhammad-Hassaan-Farooq@users.noreply.github.com>
2023-11-03 20:19:27 +0500

    Changing Welcome

--- app/src/main/java/com/example/diceroller/WelcomePage.kt
+++ app/src/main/java/com/example/diceroller/WelcomePage.kt
@@ -1,5 +1,6 @@
 package com.example.diceroller
 
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -7,8 +8,8 @@ import androidx.compose.ui.Modifier
 
 @Composable
 fun WelcomePage(onbuttonclick: () -> Unit, modifier: Modifier) {
-    Text(text = "Welcome to Dice Roller!", modifier = modifier)
-    Button(onClick = onbuttonclick,modifier= modifier) {
+    Text(text = "Welcome to Dice Roller!", modifier = modifier.fillMaxSize(0.5f))
+    Button(onClick = onbuttonclick,modifier= modifier.fillMaxSize(0.5f)) {
         Text(text = "Go")
     }
 }
